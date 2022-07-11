8d9f6bf580ae6a8a805b3a23de87340f0ea66878
amnabilr <andinabil2001@gmail.com>
2022-07-11 17:49:52 +0700

    firsit commit

---
+++ resources/views/about.blade.php
@@ -0,0 +1,14 @@
+@extends('layout/main')
+
+@section('title', 'Universitas Gunadarma | About')
+    
+@section('container')
+        <div class="container">
+            <div class="row">
+                <div class="col-10">
+                    <h1 class="mt-3">Hello, Nabil!</h1>
+                </div>
+            </div>
+        </div>
+
+@endsection
